96d07b5ff1f0a42a8b463aed5749e5d0a76b376a
Jeff Martin <jeff@reportmill.com>
2020-05-18 08:24:03 -0500

    Slight fixes.

---
+++ src/main/java/tvmapps/TVSnapBuilder.java
@@ -0,0 +1,15 @@
+package tvmapps;
+import snap.viewx.TextPane;
+
+public class TVSnapBuilder {
+
+    /**
+     * Standard main method.
+     */
+    public static void main(String[] args)
+    {
+        snaptea.TV.set();
+
+        snapbuild.app.App.main(args);
+    }
+}
