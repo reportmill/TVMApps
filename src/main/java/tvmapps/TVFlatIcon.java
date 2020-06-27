6eefb84e07a3bdf6eada13cd33ea74766b418f7c
Jeff Martin <jeff@reportmill.com>
2020-06-27 15:03:14 -0500

    Some fixes.

---
+++ src/main/java/tvmapps/TVFlatIcon.java
@@ -0,0 +1,14 @@
+package tvmapps;
+
+public class TVFlatIcon {
+
+    /**
+     * Standard main method.
+     */
+    public static void main(String[] args)
+    {
+        snaptea.TV.set();
+
+        snapbuild.app.FlatIconPanel.main(args);
+    }
+}
