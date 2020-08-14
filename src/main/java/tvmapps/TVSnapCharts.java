37303363e08486562cff962421cc81e2a1736e92
Jeff Martin <jeff@reportmill.com>
2020-08-14 11:13:41 -0500

    Slight fixes.

---
+++ src/main/java/tvmapps/TVSnapCharts.java
@@ -0,0 +1,15 @@
+package tvmapps;
+
+import snapcharts.app.App;
+
+public class TVSnapCharts {
+
+    /**
+     * Standard main method.
+     */
+    public static void main(String[] args)
+    {
+        snaptea.TV.set();
+        App.main(args);
+    }
+}
