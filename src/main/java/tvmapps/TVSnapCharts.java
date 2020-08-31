e62f4dbf5bf8949028c918175fc09bed4d9be9b9
Jeff Martin <jeff@reportmill.com>
2020-08-31 17:09:09 -0500

    Some improvements.

--- src/main/java/tvmapps/TVSnapCharts.java
+++ src/main/java/tvmapps/TVSnapCharts.java
@@ -1,5 +1,6 @@
 package tvmapps;
 
+import org.teavm.jso.JSBody;
 import snapcharts.app.App;
 
 public class TVSnapCharts {
@@ -9,7 +10,22 @@ public class TVSnapCharts {
      */
     public static void main(String[] args)
     {
+        // Get args
+        String arg0 = getMainArg0();
+        String arg1 = getMainArg1();
+        if (arg0!=null && arg0.equals("fetch") && arg1!=null) {
+            args = new String[]{arg0, arg1};
+            System.out.println("App.main: Fetching: " + arg1);
+        }
+
         snaptea.TV.set();
         App.main(args);
     }
+
+
+    @JSBody(params = { }, script = "return rmChartsMainArg0;")
+    public static native String getMainArg0();
+
+    @JSBody(params = { }, script = "return rmChartsMainArg1;")
+    public static native String getMainArg1();
 }
