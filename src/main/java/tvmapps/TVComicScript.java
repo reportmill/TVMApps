50a12ee24ee68811bb9a964130ad1aa2ae422420
Jeff Martin <jeff@reportmill.com>
2021-10-10 17:29:35 -0500

    Adding ComicScript, Puppets support

---
+++ src/main/java/tvmapps/TVComicScript.java
@@ -0,0 +1,14 @@
+package tvmapps;
+
+public class TVComicScript {
+
+    /**
+     * Standard main method.
+     */
+    public static void main(String[] args)
+    {
+        snaptea.TV.set();
+
+        comics.app.App.main(args);
+    }
+}
---
+++ src/main/java/tvmapps/TVPuppets.java
@@ -0,0 +1,14 @@
+package tvmapps;
+
+public class TVPuppets {
+
+    /**
+     * Standard main method.
+     */
+    public static void main(String[] args)
+    {
+        snaptea.TV.set();
+
+        puppets.app.App.main(args);
+    }
+}
