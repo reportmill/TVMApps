19368e260d287e0de8c2d8cddaffa3c67d3aba21
Jeff Martin <jeff@reportmill.com>
2022-09-27 14:21:59 -0500

    Some TeaVM build fixes

---
+++ src/main/java/tvmapps/TVSnapJava.java
@@ -0,0 +1,14 @@
+package tvmapps;
+
+public class TVSnapJava {
+
+    /**
+     * Standard main method.
+     */
+    public static void main(String[] args)
+    {
+        snaptea.TV.set();
+
+        snapjava.app.App.main(args);
+    }
+}
