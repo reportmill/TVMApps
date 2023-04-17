9a2134e5b9a636a80366d5f83062593b2cc9539f
Jeff Martin <jeff@reportmill.com>
2023-04-17 15:38:12 -0500

    First stab at puzzle demo

---
+++ src/main/java/tvmapps/TVPuzzle.java
@@ -0,0 +1,14 @@
+package tvmapps;
+
+public class TVPuzzle {
+
+    /**
+     * Standard main method.
+     */
+    public static void main(String[] args)
+    {
+        snaptea.TV.set();
+
+        snapdemos.puzzle.PuzzleApp.main(null);
+    }
+}
